d84cdcdf953acbbd73b1b0b43b1eed9af0acd596
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-04-11 11:55:33 -0400

    chore(internal): add test for template tag highlighting

--- tests/injections.test.js
+++ tests/injections.test.js
@@ -1,9 +1,40 @@
+import { stripIndent } from 'common-tags';
 import { parse, tag } from './-utils';
 
 describe('Injections | JS', () => {
-  test('basic', () => {
-    expect(parse('const hbs`{{foo}}`', 'js')).toEqual(
-      '<span class="hljs-keyword">const</span> hbs<span class="hljs-string">`{{foo}}`</span>'
-    );
+  describe('hbs template literal', () => {
+    test('basic', () => {
+      let result = parse(
+        stripIndent`
+          const hbs\`{{foo}}\`
+        `,
+        'js'
+      );
+
+      expect(result).toEqual(
+        '<span class="hljs-keyword">const</span> hbs<span class="hljs-string">`{{foo}}`</span>'
+      );
+    });
+  });
+
+  describe('gjs / template tag', () => {
+    test('basic', () => {
+      let result = parse(
+        stripIndent`
+          export const Name = <template>
+            {{@name}}
+          </template>;
+        `,
+        'js'
+      );
+
+      expect(result).toEqual(
+        stripIndent`
+          <span class="hljs-keyword">export</span> <span class="hljs-keyword">const</span> Name = <span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">template</span>&gt;</span>
+            {{@name}}
+          <span class="hljs-tag">&lt;/<span class="hljs-name">template</span>&gt;</span></span>;
+        `
+      );
+    });
   });
 });
