c0517f5c74a6d00a33090133b38a9a66cace1dca
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-04-11 11:48:35 -0400

    feat(grammar): support template tags in js

--- src/index.js
+++ src/index.js
@@ -26,19 +26,18 @@ function registerJavaScriptInjections(hljs) {
 
   js = js.rawDefinition(hljs);
 
-  let index = js.contains.findIndex((rule) => rule?.begin === 'css`');
-  let css = js.contains[index];
+  let cssIndex = js.contains.findIndex((rule) => rule?.begin === 'css`');
+  let css = js.contains[cssIndex];
+
+  // The default JSX grammar is actually just XML, which... is also wrong :D
+  js.contains
+    .flatMap((contains) => contains?.contains || contains)
+    .filter((rule) => rule.subLanguage === 'xml')
+    .forEach((rule) => (rule.subLanguage = 'glimmer'));
 
   const HBS_TEMPLATE = hljs.inherit(css, { begin: /hbs`/ });
 
   HBS_TEMPLATE.starts.subLanguage = 'glimmer';
-
-  const HBS_TEMPLATE_TAG = hljs.inherit(HBS_TEMPLATE, { begin: `<template>` });
-
-  HBS_TEMPLATE_TAG.starts.end = `</template>`;
-
-  js.contains.splice(index, 0, HBS_TEMPLATE);
-  js.contains.unshift(HBS_TEMPLATE_TAG);
-
+  js.contains.splice(cssIndex, 0, HBS_TEMPLATE);
   hljs.registerLanguage('javascript', () => js);
 }
