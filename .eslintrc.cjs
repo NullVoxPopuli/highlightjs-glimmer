1d1f857f49a914bf560123c54eb2cc9833dbe187
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-04-11 10:52:30 -0400

    chore(internal): lint js within html

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,11 @@
 'use strict';
 
 const { configs } = require('@nullvoxpopuli/eslint-configs');
+const {
+  jsBase,
+  moduleBase,
+  baseRulesAppliedLast,
+} = require('@nullvoxpopuli/eslint-configs/configs/base');
 
 const mjs = configs.nodeES();
 
@@ -11,11 +16,17 @@ module.exports = {
     ...mjs.overrides,
     // pull this in to eslint-configs?
     {
+      ...jsBase,
       files: ['*.html'],
-      plugins: ['html'],
+      plugins: [moduleBase.plugins, 'html'].flat(),
       parserOptions: {
         sourceType: 'module',
         ecmaVersion: 2021,
+      },
+      extends: ['eslint:recommended', 'prettier'],
+      rules: {
+        ...jsBase.rules,
+        ...baseRulesAppliedLast,
       },
     },
   ],
