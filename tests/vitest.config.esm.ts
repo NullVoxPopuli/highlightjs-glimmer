398a7009c3852f2fcdb49e886f7c161b53852e87
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-12 22:47:28 -0400

    chore(tests): switch to workspaces so that we can use exports

--- tests/vitest.config.esm.ts
+++ tests/vitest.config.esm.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     include: ['**/*.test.js'],
+  },
+  esbuild: {
+    format: 'esm',
   },
 })
